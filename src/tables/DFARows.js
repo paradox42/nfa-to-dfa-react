99cdcff98e63910be6970c2eba9a006ea8d1816d
jiansongHe <jiansonghe@live.cn>
2021-04-25 18:10:01 -0400

    another commit, pretty much done

---
+++ src/tables/DFARows.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class DFARows extends React.Component {
+  componentDidUpdate() {
+    console.log("component updated");
+  }
+  render() {
+    return <div>this is dfa rows</div>;
+  }
+}
+
+export default DFARows;
--- src/tables/DFATable.js
+++ src/tables/DFATable.js
@@ -2,37 +2,120 @@ import React from "react";
 import { isEqual } from "lodash";
 
 import "./DFATable.css";
-import NFATable from "./NFATable";
 
 class DFATable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { DFAArr: [] };
+  }
   convert = () => {
     const userInputs = this.getUserInputs();
-    this.convertToDFA(userInputs);
+    const DFAArr = this.convertToDFA(userInputs);
+    this.setState({ DFAArr: DFAArr });
   };
 
   convertToDFA = (inputJSON) => {
     var DFAArr = [];
     var firstRow = this.getOneRow(inputJSON, Object.keys(inputJSON)[0]);
     DFAArr.push(firstRow);
-    this.getAllRows(inputJSON, DFAArr, firstRow);
+    return this.getAllRows(inputJSON, DFAArr, firstRow);
   };
 
   getAllRows = (inputJSON, DFAArr, rowToMap) => {
-    var stateName = rowToMap["stateName"];
-    var transStatesOnA = rowToMap["0"];
-    var transStatesOnB = rowToMap["1"];
-    // console.log(`${stateName}: {${transStatesOnA}}, {${transStatesOnB}}`);
-    if (this.isNewState(DFAArr, transStatesOnA)) {
+    var transStatesOnA = this.removeEmpty(rowToMap["0"]);
+    var transStatesOnB = this.removeEmpty(rowToMap["1"]);
+
+    if (transStatesOnA.length && this.isNewState(DFAArr, transStatesOnA)) {
+      let newStateName = transStatesOnA;
+      let newTransStatesOnA = [];
+      let newTransStatesOnB = [];
+      transStatesOnA.forEach((state) => {
+        var transStateAForSingleState = this.getTransitionStates(
+          inputJSON,
+          state,
+          "0",
+          newTransStatesOnA
+        );
+
+        var transStateBForSingleState = this.getTransitionStates(
+          inputJSON,
+          state,
+          "1",
+          newTransStatesOnA
+        );
+
+        newTransStatesOnA = [
+          ...new Set([...newTransStatesOnA, ...transStateAForSingleState]),
+        ];
+        newTransStatesOnB = [
+          ...new Set([...newTransStatesOnB, ...transStateBForSingleState]),
+        ];
+      });
+
+      let newRow = {
+        stateName: newStateName,
+        0: newTransStatesOnA,
+        1: newTransStatesOnB,
+      };
+      DFAArr.push(newRow);
+      this.getAllRows(inputJSON, DFAArr, newRow);
     }
+
+    if (transStatesOnB.length && this.isNewState(DFAArr, transStatesOnB)) {
+      var newStateName = transStatesOnB;
+      var newTransStatesOnA = [];
+      var newTransStatesOnB = [];
+      transStatesOnB.forEach((state) => {
+        var transStateAForSingleState = this.getTransitionStates(
+          inputJSON,
+          state,
+          "0",
+          newTransStatesOnA
+        );
+
+        var transStateBForSingleState = this.getTransitionStates(
+          inputJSON,
+          state,
+          "1",
+          newTransStatesOnA
+        );
+
+        newTransStatesOnA = [
+          ...new Set([...newTransStatesOnA, ...transStateAForSingleState]),
+        ];
+        newTransStatesOnB = [
+          ...new Set([...newTransStatesOnB, ...transStateBForSingleState]),
+        ];
+      });
+
+      var newRow = {
+        stateName: newStateName,
+        0: newTransStatesOnA,
+        1: newTransStatesOnB,
+      };
+      DFAArr.push(newRow);
+      this.getAllRows(inputJSON, DFAArr, newRow);
+    }
+    return DFAArr;
   };
 
-  isNewState = (DFAArr, transStatesOnA) => {
-    DFAArr.forEach((row) => {
-      if (isEqual(row.stateName.sort(), transStatesOnA.sort())) {
-        return false;
+  removeEmpty = (arr) => {
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++) {
+      arr[i] !== "" && newArr.push(arr[i]);
+    }
+    return newArr;
+  };
+
+  isNewState = (DFAArr, arr) => {
+    var flag = true;
+    for (var i = 0; i < DFAArr.length; i++) {
+      if (isEqual(DFAArr[i].stateName.sort(), arr.sort())) {
+        flag = false;
+        break;
       }
-    });
-    return true;
+    }
+    return flag;
   };
 
   getOneRow = (inputJSON, initialState) => {
@@ -140,6 +223,51 @@ class DFATable extends React.Component {
     return arr;
   };
 
+  constructDFA = () => {
+    const rows = [];
+    this.state.DFAArr.forEach((row) => {
+      var newRow = {};
+      newRow.stateName = row.stateName;
+      var input0 = [];
+      var input1 = [];
+      if (row[0]) {
+        row[0].forEach((state) => {
+          if (state) {
+            input0.push(state);
+          } else {
+            input0.push("E");
+          }
+        });
+      } else {
+        input0 = "E";
+      }
+      if (row[1]) {
+        row[1].forEach((state) => {
+          if (state) {
+            input1.push(state);
+          } else {
+            input1.push("E");
+          }
+        });
+      } else {
+        input1 = "E";
+      }
+      newRow["0"] = input0;
+      newRow["1"] = input1;
+      rows.push(newRow);
+    });
+    console.log(this.state.DFAArr, rows);
+    return rows.map((row) => {
+      return (
+        <tr>
+          <td>{row.stateName}</td>
+          <td>{row["0"]}</td>
+          <td>{row["1"]}</td>
+        </tr>
+      );
+    });
+  };
+
   render() {
     return (
       <div>
@@ -150,15 +278,11 @@ class DFATable extends React.Component {
           <tbody>
             <tr>
               <th>State</th>
-              <th>0</th>
-              <th>1</th>
+              <th>a</th>
+              <th>b</th>
               <th>Is accepting</th>
             </tr>
-            <tr>
-              <th>[q0,q3]</th>
-              <th>[q1,q3]</th>
-              <th></th>
-            </tr>
+            {this.constructDFA()}
           </tbody>
         </table>
       </div>
